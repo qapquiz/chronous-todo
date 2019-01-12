20e2821c5b31878264fcc773913845397b2bff0b
armariya <arm.armariya@gmail.com>
2019-01-12 21:23:56 +0700

    add /

--- main.go
+++ main.go
@@ -6,8 +6,13 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", handleDefaultPath)
 	http.HandleFunc("/webhook", handleWebhook)
 	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func handleDefaultPath(w http.ResponseWriter, r *http.Request) {
+	log.Println(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
