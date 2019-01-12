70c94641015d0d6e7bc5cadedd5a315846ab5e8b
armariya <arm.armariya@gmail.com>
2019-01-12 21:19:36 +0700

    add /webhook to the server

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/webhook", handleWebhook)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Hello Webhook!")
 }
