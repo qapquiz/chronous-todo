84f4164009c4c5e6d46e87d96ec998e9ffaab042
armariya <arm.armariya@gmail.com>
2019-01-12 21:56:52 +0700

    fix the right way

--- main.go
+++ main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", handleDefaultPath)
-	http.HandleFunc("/webhook", handleWebhook)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 func handleDefaultPath(w http.ResponseWriter, r *http.Request) {
-	log.Println(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
-func handleWebhook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Hello Webhook!")
+	fmt.Fprintf(w, "Hi there, I love")
 }
